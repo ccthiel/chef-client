9fc5af4c512220735441b6db62bfb7f8440436d8
Chris Thiel <cthiel@scramsystems.com>
2018-09-11 14:35:55 -0600

    checking in workaround until 583 is fixed

--- resources/scheduled_task.rb
+++ resources/scheduled_task.rb
@@ -46,11 +46,7 @@ action :add do
 
   # This block is here due to the changes in windows_task in 13.7
   # This can be removed once we no longer support < 13.7
-  full_command = if Gem::Requirement.new('< 13.7.0').satisfied_by?(Gem::Version.new(Chef::VERSION))
-                   "cmd /c \"#{client_cmd}\""
-                 else
-                   "cmd /c \'#{client_cmd}\'"
-                 end
+  full_command = "cmd /c \"#{client_cmd}\""
 
   windows_task new_resource.task_name do
     run_level :highest
